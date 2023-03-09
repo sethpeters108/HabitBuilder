1f843a849addde8fde1058eb41d635775bc4ec5f
Shiqing Guo <72997647+ShiqingGuo@users.noreply.github.com>
2023-03-08 22:07:41 -0600

    fix dragged image size

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -11,41 +11,96 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     [SerializeField] private Canvas canvas;
 
     private RectTransform rectTransform;
-    private CanvasGroup canvasGroup;
-    private Vector2 originalPosition;
-    [SerializeField] private GameObject icon;
+    [SerializeField] private GameObject imagePrefab;
+    private Image buttonIcon;
 
-    private void Awake()
+    private Image draggedImage;
+    private RectTransform canvasRectTransform;
+    private RectTransform imageRectTransform;
+    private bool dragging;
+
+
+    void Start()
     {
-        rectTransform=GetComponent<RectTransform>();
-        canvasGroup = GetComponent<CanvasGroup>();
-        originalPosition = rectTransform.anchoredPosition;
+        buttonIcon = GetComponent<Image>();
+        canvas = FindObjectOfType<Canvas>();
+        canvasRectTransform = canvas.GetComponent<RectTransform>();
+        rectTransform = GetComponent<RectTransform>();
+
+        float width = buttonIcon.rectTransform.rect.width;
+        float height = buttonIcon.rectTransform.rect.height;
+
+        Debug.Log("Image size: " + width + " x " + height);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
-        Instantiate(icon, transform);
+        dragging = true;
+
+        // Create a new instance of the image prefab
+        GameObject imageObject = Instantiate(imagePrefab, canvas.transform);
+
+        // Get the image component from the instantiated object
+        draggedImage = imageObject.GetComponent<Image>();
+
+        // Get the RectTransform component of the image
+        imageRectTransform = imageObject.GetComponent<RectTransform>();
+
+        // Set the position of the image to match the cursor position
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, eventData.position, eventData.pressEventCamera, out localPoint);
+        imageRectTransform.anchoredPosition = localPoint;
+
+        MatchOther(imageRectTransform, rectTransform);
+        float width = imageRectTransform.rect.width;
+        float height = imageRectTransform.rect.height;
+
+        Debug.Log("big Image size: " + width + " x " + height);
+    }
+
+    public static void MatchOther(RectTransform rt, RectTransform other)
+    {
+        Vector2 myPrevPivot = rt.pivot;
+        myPrevPivot = other.pivot;
+        rt.position = other.position;
+
+        rt.localScale = other.localScale;
+
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, other.rect.width);
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, other.rect.height);
+        //rectTransf.ForceUpdateRectTransforms(); - needed before we adjust pivot a second time?
+        rt.pivot = myPrevPivot;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         //Debug.Log("OnDrag");
-        //rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
-        icon.GetComponent<RectTransform>().anchoredPosition += eventData.delta / canvas.scaleFactor;
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, eventData.position, eventData.pressEventCamera, out localPoint);
+        imageRectTransform.anchoredPosition = localPoint;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
-        canvasGroup.alpha = 1f;
-        canvasGroup.blocksRaycasts = true;
-        rectTransform.anchoredPosition = originalPosition;
+        dragging = false;
+
+        // Destroy the image when the drag ends
+        Destroy(draggedImage.gameObject);
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            // Ensure that the image stays on top of other UI elements
+        }
     }
 
 }
